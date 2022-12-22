cc5d2b26425f3d7e76bfd57097b97ff3dedfdd3b
Samantha Gosselin <samanthag2009@gmail.com>
2022-12-21 23:47:16 -0600

    altered images and button

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+window.alert("Hi");
+alert("Nice work so far.");
+alert("Also, your butt looks great today.");
+alert("But this isn't about your butt.");
+alert("(I could make it about your butt.)");
+alert("This is about clues or whatever.");
+alert("Click around the page until you get what you need.");
+alert("God, you're so good-looking.");
